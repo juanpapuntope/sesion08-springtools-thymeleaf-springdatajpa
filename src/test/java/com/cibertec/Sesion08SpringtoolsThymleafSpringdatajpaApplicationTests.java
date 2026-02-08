7ec06ad3c28190bc79436d5c0630b47544f15df6
Juan Pablo <juanpapuntope@gmail.com>
2026-02-08 09:56:17 -0500

    Primer commit

---
+++ src/test/java/com/cibertec/Sesion08SpringtoolsThymleafSpringdatajpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cibertec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sesion08SpringtoolsThymleafSpringdatajpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
